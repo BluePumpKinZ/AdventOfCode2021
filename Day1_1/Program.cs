829817ea62d43a88686c0223fb56f38fb9b20498
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-01 12:21:21 +0100

    Day 1

---
+++ Day1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day1_1 {
+	class Program {
+		static void Main (string[] args) {
+
+			List<int> data = new List<int> ();
+			string[] strings = File.ReadAllLines ("data.txt");
+			for (int i = 0; i < strings.Length; i++) {
+				data.Add (int.Parse(strings[i]));
+			}
+
+			int count = 0;
+			for (int i = 1; i < data.Count; i++) {
+				if (data[i] > data[i - 1])
+					count++;
+			}
+			Console.WriteLine (count);
+
+		}
+	}
+}
---
+++ Day1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day1_2 {
+	class Program {
+		static void Main (string[] args) {
+
+			List<int> data = new List<int> ();
+			string[] strings = File.ReadAllLines ("data.txt");
+			for (int i = 0; i < strings.Length; i++) {
+				data.Add (int.Parse (strings[i]));
+			}
+
+			int count = 0;
+			for (int i = 3; i < data.Count; i++) {
+				int current = data[i] + data[i - 1] + data[i - 2];
+				int last = data[i - 1] + data[i - 2] + data[i - 3];
+				if (current > last)
+					count++;
+			}
+			Console.WriteLine (count);
+
+		}
+	}
+}
