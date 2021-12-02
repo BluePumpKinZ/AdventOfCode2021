76feb287fa1cf485bfb6c92ec03456a63ffecb2d
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-02 09:34:58 +0100

    Day 2

---
+++ Day2_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Day2_1 {
+	class Program {
+		static void Main (string[] args) {
+			int forward = 0;
+			int depth = 0;
+
+			foreach (string s in File.ReadAllLines ("data.txt")) {
+				string[] split = s.Split (' ');
+				int amount = int.Parse (split[1]);
+				switch (split[0]) {
+				case "forward":
+					forward += amount;
+					break;
+				case "up":
+					depth -= amount;
+					break;
+				case "down":
+					depth += amount;
+					break;
+				}
+			}
+			Console.WriteLine (forward * depth);
+		}
+	}
+}
---
+++ Day2_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Day2_1 {
+	class Program {
+		static void Main (string[] args) {
+			long forward = 0;
+			long depth = 0;
+			long aim = 0;
+
+			foreach (string s in File.ReadAllLines ("data.txt")) {
+				string[] split = s.Split (' ');
+				int amount = int.Parse (split[1]);
+				switch (split[0]) {
+				case "forward":
+					forward += amount;
+					depth += aim * amount;
+					break;
+				case "up":
+					aim -= amount;
+					break;
+				case "down":
+					aim += amount;
+					break;
+				}
+			}
+			Console.WriteLine (forward * depth);
+		}
+	}
+}
