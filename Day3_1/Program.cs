4bbebe3ac097bfee4cae3bb0b531cea91e5c5cd4
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-05 01:10:58 +0100

    Day 3

---
+++ Day3_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day2_1 {
+	class Program {
+		static void Main (string[] args) {
+
+			int[] counts = new int[12];
+			string[] strings = File.ReadAllLines ("data.txt");
+			for (int i = 0; i < strings.Length; i++) {
+				for (int j = 0; j < 12; j++) {
+					switch (strings[i][j]) {
+						case '0':
+							counts[j] += 1;
+							break;
+						case '1':
+							counts[j] -= 1;
+							break;
+					}
+				}
+			}
+			int total = 0;
+			int andereTotal = 0;
+			for (int i = 0; i < 12; i++) {
+				total <<= 1;
+				total += counts[i] > 0 ? 0 : 1;
+				andereTotal <<= 1;
+				andereTotal += counts[i] < 0 ? 0 : 1;
+			}
+			Console.WriteLine (andereTotal * total);
+		}
+	}
+}
---
+++ Day3_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Day2_1 {
+	class Program {
+		static void Main (string[] args) {
+
+			int digits = 12;
+			// int[] counts = new int[12];
+			List<string> strings = File.ReadAllLines ("data.txt").ToList ();
+			for (int i = 0; i < digits; i++) {
+				int bias = 0;
+				strings.ForEach (s => bias += s[i] == '1' ? 1 : -1);
+				strings = strings.Where (s => s[i] == (bias >= 0 ? '1' : '0')).ToList ();
+				if (strings.Count == 1)
+					break;
+			}
+			string result = strings[0];
+
+			strings = File.ReadAllLines ("data.txt").ToList ();
+			for (int i = 0; i < digits; i++) {
+				int bias = 0;
+				strings.ForEach (s => bias += s[i] == '1' ? 1 : -1);
+				strings = strings.Where (s => s[i] == (bias >= 0 ? '0' : '1')).ToList ();
+				if (strings.Count == 1)
+					break;
+			}
+			string result1 = strings[0];
+
+			int total = 0;
+			int andereTotal = 0;
+			for (int i = 0; i < digits; i++) {
+				total <<= 1;
+				total += result[i] == '0' ? 0 : 1;
+				andereTotal <<= 1;
+				andereTotal += result1[i] == '0' ? 0 : 1;
+			}
+			Console.WriteLine (total * andereTotal);
+		}
+	}
+}
