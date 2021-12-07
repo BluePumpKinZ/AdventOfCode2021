8f0356ef31a4f271f667c025e893b31b5b9c705e
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-07 01:52:00 +0100

    Day 6

---
+++ Day6_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day6_1 {
+	class Program {
+		static void Main (string[] args) {
+
+			string data = File.ReadAllText ("data.txt");
+			
+			string[] dataStrings = data.Split (",");
+			List<long> fishes = new List<long> (dataStrings.Length);
+			for (int i = 0; i < dataStrings.Length; i++) {
+				fishes.Add (int.Parse (dataStrings[i]));
+			}
+
+			for (int i = 0; i < 80; i++) {
+				long newFishes = 0;
+				for (int j = 0; j < fishes.Count; j++) {
+					fishes[j]--;
+					if (fishes[j] == -1) {
+						fishes[j] = 6;
+						newFishes++;
+					}
+				}
+				for (int j = 0; j < newFishes; j++)
+					fishes.Add (8);
+				
+			}
+			Console.WriteLine (fishes.Count);
+
+		}
+	}
+}
---
+++ Day6_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day6_2 {
+	class Program {
+		static void Main (string[] args) {
+			string data = File.ReadAllText ("data.txt");
+
+			string[] dataStrings = data.Split (",");
+
+			long[] fishes = new long[9];
+			for (int i = 0; i < dataStrings.Length; i++) {
+				fishes[int.Parse (dataStrings[i])]++;
+			}
+
+			for (int i = 0; i < 256; i++) {
+				long zeros = fishes[0];
+				for (int j = 1; j <= 8; j++) {
+					fishes[j - 1] = fishes[j];
+				}
+				fishes[6] += zeros;
+				fishes[8] = zeros;
+			}
+
+			long total = 0;
+			for (int i = 0; i < fishes.Length; i++)
+				total += fishes[i];
+
+			Console.WriteLine (total);
+		}
+	}
+}
