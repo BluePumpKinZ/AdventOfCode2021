e64415bbf59782f69976c553f89966e0444c532c
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-13 01:22:46 +0100

    Day 11

---
+++ Day11_2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+
+namespace Day11_2 {
+	class Program {
+
+		private static int total;
+
+		static void Main () {
+
+			string[] data = File.ReadAllLines ("data.txt");
+			int[][] grid = new int[10][];
+
+			for (int i = 0; i < grid.Length; i++) {
+				grid[i] = new int[10];
+				for (int j = 0; j < 10; j++)
+					grid[i][j] = int.Parse (data[i][j].ToString ());
+			}
+
+			for (int i = 0; i < 1000; i++) {
+				bool[][] flashed = new bool[10][];
+
+				for (int j = 0; j < 10; j++)
+					flashed[j] = new bool[10];
+
+				for (int y = 0; y < 10; y++) {
+					for (int x = 0; x < 10; x++) {
+						IncreaseCell (grid, flashed, x, y);
+					}
+				}
+
+				if (AllFlash (grid)) {
+					Console.WriteLine (i + 1);
+					return;
+				}
+
+				for (int y = 0; y < 10; y++) {
+					for (int x = 0; x < 10; x++) {
+						if (grid[y][x] > 9) {
+							grid[y][x] = 0;
+						}
+					}
+				}
+			}
+			Console.WriteLine (total);
+		}
+
+		private static bool AllFlash (int[][] grid) {
+			for (int y = 0; y < 10; y++) {
+				for (int x = 0; x < 10; x++) {
+					if (grid[y][x] <= 9) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		private static void IncreaseCell (int[][] grid, bool[][] flashed, int _x, int _y) {
+			grid[_y][_x]++;
+
+			if (grid[_y][_x] != 10)
+				return;
+
+			flashed[_y][_x] = true;
+			total++;
+			int xMin = Math.Max (0, _x - 1);
+			int xMax = Math.Min (9, _x + 1);
+
+			int yMin = Math.Max (0, _y - 1);
+			int yMax = Math.Min (9, _y + 1);
+			for (int x = xMin; x <= xMax; x++) {
+				for (int y = yMin; y <= yMax; y++) {
+					if (flashed[y][x])
+						continue;
+					IncreaseCell (grid, flashed, x, y);
+				}
+			}
+		}
+
+		private static bool HasNines (int[][] grid) {
+			for (int y = 0; y < 10; y++) {
+				for (int x = 0; x < 10; x++) {
+					if (grid[y][x] == 9)
+						return true;
+				}
+			}
+			return false;
+		}
+	}
+}
