81b186cb99cd877d2f8f3369c51d6dd1793229d9
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-15 14:07:39 +0100

    Day 14 Part 2 unfinished

---
+++ Day14_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day14_1 {
+	class Program {
+		static void Main (string[] args) {
+
+			string[] data = File.ReadAllLines ("data.txt");
+
+			List<char> workingString = data[0].ToCharArray ().ToList ();
+
+			List<(string match, char insert)> replacements = new List<(string, char)> ();
+
+			for (int i = 2; i < data.Length; i++) {
+				string[] split = data[i].Split (" -> ");
+				replacements.Add ((split[0], split[1][0]));
+			}
+
+			for (int i = 0; i < 10; i++) {
+				for (int j = 0; j < workingString.Count - 1; j++) {
+					string toMatch = workingString[j] + "" + workingString[j + 1];
+					for (int k = 0; k < replacements.Count; k++) {
+						if (toMatch == replacements[k].match) {
+							workingString.Insert (j + 1, replacements[k].insert);
+							j++;
+						}
+					}
+				}
+			}
+
+			List<(char c, int count)> occurences = new List<(char, int)> ();
+			foreach (var a in workingString.GroupBy (t => t)) {
+				Console.WriteLine ($"{a.Key} {a.Count ()}");
+				occurences.Add ((a.Key, a.Count ()));
+			}
+
+			occurences.Sort (delegate ((char c, int count) a, (char c, int count) b) { return a.count.CompareTo (b.count); });
+			Console.WriteLine (occurences[^1].count - occurences[0].count);
+		}
+	}
+}
---
+++ Day14_2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day14_2 {
+	class Program {
+
+		private static List<(string match, char insert)> replacements;
+
+		static void Main (string[] args) {
+
+			string[] data = File.ReadAllLines ("data.txt");
+
+			string startString = data[0];
+
+			replacements = new List<(string, char)> ();
+
+			for (int i = 2; i < data.Length; i++) {
+				string[] split = data[i].Split (" -> ");
+				replacements.Add ((split[0], split[1][0]));
+			}
+
+			Dictionary<char, int> occurences = CountChars (startString, 10);
+			List<(char c, int count)> occurencesList = new List<(char, int)> ();
+			foreach (KeyValuePair<char, int> kvp in occurences)
+				occurencesList.Add ((kvp.Key, kvp.Value));
+
+			occurencesList.Sort ((a, b) => a.count.CompareTo (b.count));
+			Console.WriteLine (occurencesList[^1].count - occurencesList[0].count);
+		}
+
+		private static Dictionary<char, int> CountChars (string input, int iterationsToGo) {
+			Dictionary<char, int> dic = new Dictionary<char, int> ();
+			if (iterationsToGo == 0) {
+				AddToDictionary (dic, input[0], 1);
+				AddToDictionary (dic, input[1], 1);
+				return dic;
+			}
+			iterationsToGo--;
+			Dictionary<char, int> dicsList = new Dictionary<char, int> ();
+			for (int i = 0; i < replacements.Count; i++) {
+				if (input == replacements[i].match)
+					dicsList.Add (CountChars (input, iterationsToGo));
+			}
+			dic = SumDictionaries (dicsList);
+			return dic;
+		}
+
+		private static void AddToDictionary (Dictionary<char, int> dic, char c, int count) {
+			dic.TryGetValue (c, out int ogValue);
+			ogValue += count;
+			dic[c] = ogValue;
+		}
+
+		private static Dictionary<char, int> SumDictionaries (Dictionary<char, int>[] dics) {
+			Dictionary<char, int> sum = new Dictionary<char, int> ();
+			for (int i = 0; i < dics.Length; i++) {
+				foreach (KeyValuePair<char, int> kvp in dics[i]) {
+					AddToDictionary (sum, kvp.Key, kvp.Value);
+				}
+			}
+
+			return sum;
+
+		}
+	}
+}
