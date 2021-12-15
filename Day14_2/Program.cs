1af05d76540550659ee0e68d044e59bad496f069
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-15 17:40:21 +0100

    Day 14

--- Day14_2/Program.cs
+++ Day14_2/Program.cs
@@ -21,42 +21,75 @@ namespace Day14_2 {
 				replacements.Add ((split[0], split[1][0]));
 			}
 
-			Dictionary<char, int> occurences = CountChars (startString, 10);
-			List<(char c, int count)> occurencesList = new List<(char, int)> ();
-			foreach (KeyValuePair<char, int> kvp in occurences)
+			Dictionary<char, long> sum = new Dictionary<char, long> ();
+			for (int i = 0; i < startString.Length - 1; i++) {
+				sum = SumDictionaries (sum, CountCharsCached (startString[i] + "" + startString[i + 1], 40));
+			}
+			AddToDictionary (sum, startString[^1], 1);
+			List<(char c, long count)> occurencesList = new List<(char, long)> ();
+			foreach (KeyValuePair<char, long> kvp in sum)
 				occurencesList.Add ((kvp.Key, kvp.Value));
 
 			occurencesList.Sort ((a, b) => a.count.CompareTo (b.count));
 			Console.WriteLine (occurencesList[^1].count - occurencesList[0].count);
 		}
 
-		private static Dictionary<char, int> CountChars (string input, int iterationsToGo) {
-			Dictionary<char, int> dic = new Dictionary<char, int> ();
+		private static Dictionary<(string input, int iterationsToGo), Dictionary<char, long>> cache = new Dictionary<(string input, int iterationsToGo), Dictionary<char, long>> ();
+		private static Dictionary<char, long> CountCharsCached (string input, int iterationsToGo) {
+
+			var key = (input, iterationsToGo);
+
+			if (cache.TryGetValue (key, out Dictionary<char, long> dic)) {
+				return dic;
+			}
+			else {
+				Dictionary<char, long> newValue = CountChars (input, iterationsToGo);
+				cache.Add (key, newValue);
+
+				return newValue;
+			}
+		}
+
+		private static Dictionary<char, long> CountChars (string input, int iterationsToGo) {
 			if (iterationsToGo == 0) {
+				Dictionary<char, long> dic = new Dictionary<char, long> ();
 				AddToDictionary (dic, input[0], 1);
-				AddToDictionary (dic, input[1], 1);
+				// AddToDictionary (dic, input[1], 1);
 				return dic;
 			}
 			iterationsToGo--;
-			Dictionary<char, int> dicsList = new Dictionary<char, int> ();
+
+			(string match, char insert) replacement = default;
 			for (int i = 0; i < replacements.Count; i++) {
-				if (input == replacements[i].match)
-					dicsList.Add (CountChars (input, iterationsToGo));
+				string match = replacements[i].match;
+				if (input == match) {
+					replacement = replacements[i];
+					break;
+				}
 			}
-			dic = SumDictionaries (dicsList);
-			return dic;
+
+			if (replacement != default) {
+
+				Dictionary<char, long> a = CountCharsCached (input[0] + "" + replacement.insert, iterationsToGo);
+				Dictionary<char, long> b = CountCharsCached (replacement.insert + "" + input[1], iterationsToGo);
+
+				return SumDictionaries (a, b);
+
+			}
+
+			return CountChars (input, 0);
 		}
 
-		private static void AddToDictionary (Dictionary<char, int> dic, char c, int count) {
-			dic.TryGetValue (c, out int ogValue);
+		private static void AddToDictionary (Dictionary<char, long> dic, char c, long count) {
+			dic.TryGetValue (c, out long ogValue);
 			ogValue += count;
 			dic[c] = ogValue;
 		}
 
-		private static Dictionary<char, int> SumDictionaries (Dictionary<char, int>[] dics) {
-			Dictionary<char, int> sum = new Dictionary<char, int> ();
+		private static Dictionary<char, long> SumDictionaries (params Dictionary<char, long>[] dics) {
+			Dictionary<char, long> sum = new Dictionary<char, long> ();
 			for (int i = 0; i < dics.Length; i++) {
-				foreach (KeyValuePair<char, int> kvp in dics[i]) {
+				foreach (KeyValuePair<char, long> kvp in dics[i]) {
 					AddToDictionary (sum, kvp.Key, kvp.Value);
 				}
 			}
