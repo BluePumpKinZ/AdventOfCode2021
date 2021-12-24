0641599924b177ea12543b8be9a3ffbc4dd98a2a
Jonas Leijzen <jonasleijzen@gmail.com>
2021-12-24 17:33:25 +0100

    Day 17

---
+++ Day17_1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Day17_1 {
+	class Program {
+
+		static int xMin = 281;
+		static int xMax = 311;
+
+		static int yMin = -74;
+		static int yMax = -54;
+
+		static void Main (string[] args) {
+
+			// target area: x=281..311, y=-74..-54
+
+			int maxY = 0;
+
+			for (int i = 0; i < 100; i++) {
+				for (int j = 0; j < i; j++) {
+					int xVel = i - j;
+					int yVel = j;
+					if (IsValid (xVel, yVel, out int highestY)) {
+						maxY = Math.Max (highestY, maxY);
+					}
+				}
+				Console.WriteLine (i);
+			}
+			Console.WriteLine (maxY);
+		}
+
+		static bool IsValid (int xVel, int yVel, out int maxY) {
+
+			int xPos = 0;
+			int yPos = 0;
+
+			maxY = 0;
+
+			while (yPos >= yMin) {
+
+				xPos += xVel;
+				yPos += yVel;
+
+				maxY = Math.Max (maxY, yPos);
+
+				switch (xVel) {
+				case > 0:
+					xVel--;
+					break;
+				case < 0:
+					xVel++;
+					break;
+				}
+
+				yVel--;
+
+				if (xMin <= xPos && xPos <= xMax && yMin <= yPos && yPos <= yMax) {
+					return true;
+				}
+
+
+			}
+
+			return false;
+		}
+	}
+}
---
+++ Day17_2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Day17_2 {
+	class Program {
+
+		static int xMin = 281;
+		static int xMax = 311;
+
+		static int yMin = -74;
+		static int yMax = -54;
+
+		static void Main (string[] args) {
+
+			// target area: x=281..311, y=-74..-54
+
+			int count = 0;
+
+			for (int i = 0; i < 500; i++) {
+				for (int j = -100; j < 1000; j++) {
+					int xVel = i;
+					int yVel = j;
+					if (IsValid (xVel, yVel)) {
+						count++;
+					}
+				}
+				Console.WriteLine (i);
+			}
+			Console.WriteLine (count);
+		}
+
+		static bool IsValid (int xVel, int yVel) {
+
+			int xPos = 0;
+			int yPos = 0;
+
+			while (yPos >= yMin) {
+
+				xPos += xVel;
+				yPos += yVel;
+
+				switch (xVel) {
+					case > 0:
+						xVel--;
+						break;
+					case < 0:
+						xVel++;
+						break;
+				}
+
+				yVel--;
+
+				if (xMin <= xPos && xPos <= xMax && yMin <= yPos && yPos <= yMax) {
+					return true;
+				}
+
+
+			}
+
+			return false;
+		}
+	}
+}
